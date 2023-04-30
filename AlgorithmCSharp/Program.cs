a61f3389d83fc2e26dd5761025191a8e255f7449
zihan.windowsPC <hhhhhunger98@gmail.com>
2023-04-30 12:31:27 +0200

    list problems 203,707,206,24,19 solved

--- AlgorithmCSharp/Program.cs
+++ AlgorithmCSharp/Program.cs
@@ -1,7 +1,190 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using static AlgorithmCSharp.AlgorithmSolution;
 
 namespace AlgorithmCSharp
 {
+    #region 707. Design Linked List
+    public class MyLinkedList
+    {
+        public int? val;
+        public MyLinkedList? next;
+        public int listSize;
+        public MyLinkedList() 
+        {
+            val = null;
+            next = null;
+            listSize = 0;
+        }
+        public MyLinkedList(int? val)
+        {
+            this.val = val;
+            next = null;
+        }
+
+        public int Get(int index)
+        {
+            if (index < 0 || index >= listSize) return -1;
+            MyLinkedList dummyHead = new MyLinkedList();
+            dummyHead.val = val;
+            dummyHead.next = this;
+
+            MyLinkedList tmp = dummyHead;
+            while (index-- > 0) 
+            {
+                if (tmp.next != null) tmp = tmp.next;
+                else return -1;
+            }
+            return tmp.next != null ? (int)tmp.next.val : -1;
+        }
+
+        public void AddAtHead(int val)
+        {
+            listSize++;
+            if (this.val == null) { this.val = val; return; }
+            MyLinkedList? tmp = new MyLinkedList(this.val);
+            tmp.next = next;
+            this.val = val;
+            next = tmp;
+        }
+
+        public void AddAtTail(int val)
+        {
+            // Add a virtual head node called dummyhead
+            MyLinkedList dummyHead = new MyLinkedList();
+            dummyHead.val = val;
+            dummyHead.next = this;
+            listSize++;
+            
+            if (this.val == null) { this.val = val; return; }
+            MyLinkedList tmp = dummyHead;
+            while (tmp.next != null)
+            {
+                tmp = tmp.next;
+            }
+            tmp.next = new MyLinkedList(val);
+        }
+
+        public void AddAtIndex(int index, int val)
+        {
+            if (index < 0 || index > listSize) { return; }
+            else if (index == listSize)
+            {
+                AddAtTail(val); return;
+            }
+            // Add a virtual head node called dummyhead
+            MyLinkedList dummyHead = new MyLinkedList();
+            dummyHead.val = val;
+            dummyHead.next = this;
+            listSize++;
+
+            MyLinkedList tmp = dummyHead;
+            while (index-- > 0)
+            {
+                tmp = tmp.next;
+            }
+
+            if (this.val != null)
+            {
+                MyLinkedList newNode = new MyLinkedList(tmp.next.val);
+                newNode.next = tmp.next.next;
+                tmp.next.next = newNode;
+                tmp.next.val = val;
+            }else
+            {
+                this.val = val; 
+            }
+
+        }
+
+        public void DeleteAtIndex(int index)
+        {
+            if (index < 0 || index >= listSize) { return; }
+            if (index == 0 && next != null)
+            {
+                val = next.val;
+                next = next.next;
+                listSize--;
+                return;
+            }
+            // Add a virtual head node called dummyhead
+            MyLinkedList dummyHead = new MyLinkedList();
+            dummyHead.val = val;
+            dummyHead.next = this;
+
+            MyLinkedList tmp = dummyHead;
+            
+            while (index-- > 0)
+            {
+                tmp = tmp.next;
+            }
+
+            if (listSize-- == 1)
+            {
+                tmp.next = this;
+                tmp.next.next = null;
+                tmp.val = null;
+                return;
+            }
+            else
+            {
+                tmp.next = tmp.next.next == null ? null : tmp.next.next;
+            }
+        }
+        public static void PrintList(MyLinkedList head)
+        {
+            MyLinkedList? tmp = head;
+            Console.Write("The list is :");
+            while (tmp != null)
+            {
+                Console.Write(tmp.val + " ");
+                if (tmp.val == null) Console.Write("damn");
+                tmp = tmp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+    #endregion
+
+    //LinkedList Definition
+    public class ListNode
+    {
+        /* Definition of a list struct
+         * 
+         * unsafe public struct MyList
+        {
+            int val;
+            MyList* next;
+
+            public MyList() { this.val = 0; this.next = null; }
+            public MyList(int val) : this() { this.val = val; this.next = null; }
+        }*/
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public void AddNode(ListNode nodeToBeAdded) 
+        {
+            ListNode tmp = this;
+            while(tmp.next != null) tmp = tmp.next;
+            tmp.next = nodeToBeAdded;
+        }
+        public static void PrintList(ListNode head)
+        {
+            ListNode tmp = head;
+            Console.Write("The list is :");
+            while(tmp != null)
+            {
+                Console.Write(tmp.val + " ");
+                tmp = tmp.next;
+            }
+            Console.WriteLine();
+        }
+    }
     public static class TestCaseGenerator
     {
         public static List<int> IntListGenerator(int maxLen, int maxVal)
@@ -45,9 +228,49 @@ namespace AlgorithmCSharp
 
             return list;
         }
+        public static ListNode ListNodesGenerator(int maxLen, int maxVal)
+        {
+            Random rnd = new Random();
+            ListNode list = new ListNode(rnd.Next(maxVal));
+            int len = rnd.Next(maxLen);
+            Console.WriteLine("The length of the list is:" + len);
+            if ( len == 0) return null;
+
+            while (--len > 0)
+            {
+                list.AddNode(new ListNode(rnd.Next(maxVal)));
+            }
+            
+            ListNode tmp = list;
+            Console.WriteLine("The generated list is:");
+            while (tmp != null)
+            {
+                Console.Write(tmp.val + " ");
+                tmp = tmp.next;
+            }
+            Console.WriteLine();
+
+            return list;
+        }
+        public static MyLinkedList MyListGenerator(int maxLen, int maxVal)
+        {
+            Random rnd = new Random();
+            MyLinkedList tmp = new MyLinkedList(rnd.Next(maxVal));
+            MyLinkedList head = tmp;
+            int len = rnd.Next(maxLen);
+            Console.WriteLine("The length is: " + len);
+            for (int i = 0; i < len; i++)
+            {
+                tmp.next = new MyLinkedList(rnd.Next(maxVal));
+                tmp = tmp.next;
+            }
+
+            return head.next;
+        }
     }
     public static class AlgorithmSolution
     {
+        #region Array Problems
         #region 4. Median of Two Sorted Array
         public static double FindMedian (int[] nums)
         {
@@ -301,11 +524,96 @@ namespace AlgorithmCSharp
             return matrix;
         }
         #endregion
+        #endregion
+
+        #region List Problems
+        #region 203. Remove Linked List Elements
+        public static ListNode RemoveElements(ListNode head, int val)
+        {
+            ListNode current;
+            ListNode last = head;
+            while(last != null)
+            {
+                current = last.next;
+                if (last.val == val && head == last) { head = last.next; last = last.next; continue; }
+                else if (current == null) break;
+                else if (current.val == val) last.next = current.next;
+                else last = current;
+            }
+
+            ListNode.PrintList(head);
+            return head;
+        }
+        #endregion
+
+        #region 707. Design Linked List
+        // See the public class MyLinkedList
+        #endregion
+
+        #region 206. Reverse Linked List
+        // See the public class ListNode
+        public static ListNode ReverseList(ListNode head)
+        {
+            if (head == null) return head;
+
+            ListNode curr = head, prev = null, tmp =null;
+
+            while (curr != null)
+            {
+                tmp = curr;
+                curr = curr.next;
+                tmp.next = prev;
+                prev = tmp;
+            }
+            
+            return prev;
+        }
+        #endregion
+
+        #region 24. Swap Nodes in Pairs
+        public static ListNode SwapPairs(ListNode head)
+        {
+            ListNode curr = head;
+            while (curr != null && curr.next != null)
+            {
+                int tmp = curr.val;
+                curr.val = curr.next.val;
+                curr.next.val = tmp;
+                curr = curr.next.next;
+            }
+            return head;
+        }
+        #endregion
+
+        #region 19. Remove Nth Node From End Of The List
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode dummyHead = new ListNode();
+            dummyHead.next = head;
+
+            ListNode fast = dummyHead, slow = dummyHead;
+            for (int i = 0; i < n; i++)
+            {
+                if (fast.next == null) return dummyHead.next;
+                fast = fast.next;
+            }
+
+            while(fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+            return dummyHead.next;
+        }
+        #endregion
+        #endregion
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Array Problems
             #region 4. Median of Two Sorted Array
             /*int[] arr1 = TestCaseGenerator.IntArrayGenerator(7, 100);
             int[] arr2 = TestCaseGenerator.IntArrayGenerator(10, 100);
@@ -346,6 +654,7 @@ namespace AlgorithmCSharp
             #endregion
 
             #region 59. Spiral Matrix II
+            /*//real Solution
             int[][] matrix = AlgorithmSolution.GenerateMatrix(3);
             for (int i = 0; i < matrix.Length; i++)
             {
@@ -356,6 +665,7 @@ namespace AlgorithmCSharp
                 Console.WriteLine();
             }
 
+            //Test the matrix's elements position
             int[][] mt = new int[3][];
             for (int i = 0, val = 0 ; i < mt.Length; i++)
             {
@@ -366,7 +676,50 @@ namespace AlgorithmCSharp
                     Console.Write("ij:" + i + " " + j + " "+ mt[i][j] + " ");
                 }
                 Console.WriteLine();
-            } 
+            }*/
+            #endregion
+            #endregion
+
+            #region Linked List Problems
+            #region 203. Remove Linked List Elements
+            /*ListNode head = TestCaseGenerator.ListNodesGenerator(10, 20);
+            RemoveElements(head, 10);*/
+            #endregion
+
+            #region 707. Design Linked List
+            /*MyLinkedList myList = TestCaseGenerator.MyListGenerator(3, 20);
+            MyLinkedList.PrintList(myList);
+
+            if (myList != null) Console.WriteLine(myList.Get(3));*/
+            /*
+                        Console.WriteLine("111111111111111111111111");
+
+                        MyLinkedList list2 = new MyLinkedList();
+                        list2.AddAtHead(1);
+                        list2.AddAtTail(3);
+                        list2.AddAtIndex(1, 2);
+                        MyLinkedList.PrintList(list2);
+                        Console.WriteLine(list2.Get(1) + " size " + list2.listSize);
+                        list2.DeleteAtIndex(0);
+                        MyLinkedList.PrintList(list2);
+                        Console.WriteLine("    " + list2.Get(0) + " " + list2.listSize);
+                        MyLinkedList.PrintList(list2);*/
+            #endregion
+
+            #region 206. Reverse Linked List
+            /*ListNode myListNode = TestCaseGenerator.ListNodesGenerator(5, 20);
+            ListNode.PrintList(AlgorithmSolution.ReverseList(myListNode));*/
+            #endregion
+
+            #region 24. Swap Nodes in Pairs
+            /*ListNode myListNode = TestCaseGenerator.ListNodesGenerator(2, 20);
+            ListNode.PrintList(AlgorithmSolution.SwapPairs(myListNode));*/
+            #endregion
+
+            #region 19. Remove Nth Node From End Of The List
+            ListNode myListNode = TestCaseGenerator.ListNodesGenerator(20, 20);
+            ListNode.PrintList(AlgorithmSolution.RemoveNthFromEnd(myListNode, 5));
+            #endregion
             #endregion
         }
     }
